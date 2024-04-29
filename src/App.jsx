c10c7e39cf161e866755d8829c696dedd8e44d18
Lucas Grohe Gomes <lucas_g_gomes@estudante.sesisenai.org.br>
2024-04-29 12:04:49 -0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,12 @@ function App() {
   return (
     <main>
       <Banner />
-      <Event />
+      <Event 
+      titulo = {"Evento de Eventos"}
+      descricao = {"Este é um evento onde clipes de eventos serão apresentados, os eventos serão avaliados por especialistas e técnicos em eventos para julgar o mais maneiro"}
+      hora = {"13/02/2024, 13:00h"}
+      local = {"Rua José Garibaldi, Uruguaiana RS"}
+      />
       <Galeria />
       <Footer />
     </main>
--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -1,25 +1,16 @@
 import React from 'react';
 
-export default function Event(){
+export default function Event({titulo, descricao, hora, local}){
     return(
-        <section>
-        <div className='section-container'>
-            <div>
-            <h2>Atividades / Eventos</h2>
-            <br></br>
-            <h3>"Evento sobre  eventos".</h3>
-            <br></br>
-            <p>Este é um evento onde o principal foco será a apresentação de vídeos sobre eventos e convenções especiais, os vídeos mais interessantes receberão mais prêmios.</p>
-            <br></br>
-            <h5>Local da competição de eventos: Rua Catorze de Julho, Uruguaiana RS ; 97510-450</h5>
-            <h5>12.00h de 01/04/2025</h5>
-            <br></br>
-            </div>
-            <div>
-            <img src="./src/images/evento.jpg"></img>
-            </div>
+        <section className='section-container'>
+        <div>
+            <p>{titulo}</p>
+            <p>{descricao}</p>
+            <p>{hora}</p>
+            <p>{local}</p>
         </div>
             
+            <img src="./src/images/evento.jpg"></img>
       
         </section>
     )
